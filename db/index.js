123d3d562372a2c7a2c26f9eb63044c32df563f7
Yun <yuniya.pa98@gmail.com>
2022-09-18 12:18:23 -0400

    atlas connect

--- db/index.js
+++ db/index.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
+
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/<database_name>'
 
 mongoose
 
-  .connect('mongodb://127.0.0.1:27017/GardenDB')
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
