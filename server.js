43935a18514ac14151d58cc7ab524a2510b1735c
Yun <yuniya.pa98@gmail.com>
2022-09-19 19:56:34 -0400

    fixing server.js for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
+const path = require('path')
 const cors = require('cors')
 const logger = require('morgan')
 const PORT = process.env.PORT || 3001
@@ -10,14 +11,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(`${__dirname}/client/my-garden/build`))
+app.use(express.static(path.join(__dirname, 'build')))
 app.use(logger('dev'))
 
 app.use('/api', routes)
 // db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/client/my-garden/build/index.html`)
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(PORT, () => console.log('server is running at PORT', PORT))
