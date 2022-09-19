3e48e1043ecf1930ccb7e4abfea8185b5843a1d4
Yun <yuniya.pa98@gmail.com>
2022-09-19 18:05:06 -0400

    server.js file fixed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(logger('dev'))
 app.use('/api', routes)
 // db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/*', (req, res) => {
-  res.sendFile(`${__dirname}/client/public/index.html`)
+  res.sendFile(`${__dirname}/client/build/index.html`)
 })
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`)
