eaa0f5dc8c2893fae6d586b5ea033b3988d72e2f
Yun <yuniya.pa98@gmail.com>
2022-09-19 20:52:19 -0400

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const cors = require('cors')
 const logger = require('morgan')
 // const PORT = process.env.PORT || 3001
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 const db = require('./db')
 
 const app = express()
